64a7dba6a9d629ce2b1abe932cd39408ff5bd086
Kovacs Akos <akoskovacs1980@gmail.com>
2021-09-19 01:51:01 +0200

    Added PrimeNg, Bootstrap and OpenAPI generator to generate services from the server's swagger file.

--- src/frontend/cryptodoge-spa/src/app/app.module.ts
+++ src/frontend/cryptodoge-spa/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import { BASE_PATH } from 'generated/client';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [{ provide: BASE_PATH, useValue: environment.baseUrl }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
