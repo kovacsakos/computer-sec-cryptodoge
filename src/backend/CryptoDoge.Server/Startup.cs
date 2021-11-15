c41026962cb9f05947f056b059118533b65fd72f
Kovacs Akos <akoskovacs1980@gmail.com>
2021-11-15 01:25:12 +0100

    Added logging to the API.

--- src/backend/CryptoDoge.Server/Startup.cs
+++ src/backend/CryptoDoge.Server/Startup.cs
@@ -1,3 +1,4 @@
+using CryptoDoge.ParserService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -25,7 +26,7 @@ namespace CryptoDoge.Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IParserService, ParserService.ParserService>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
