49038ba89356c30997035317eb89267ca6a2ea0c
Rajmundo1 <rajmund.knapp@gmail.com>
2021-11-20 16:18:04 +0100

    Add Service Interfaces and the Dtos

---
+++ src/backend/CryptoDoge.BLL/Interfaces/IAuthAppService.cs
@@ -0,0 +1,17 @@
+﻿using CryptoDoge.BLL.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoDoge.BLL.Interfaces
+{
+	public interface IAuthAppService
+	{
+		Task<TokenDto> LoginAsync(LoginDto authDto);
+		Task LogoutAsync();
+		Task<TokenDto> RegisterAsync(RegisterDto authDto);
+		Task<TokenDto> RenewTokenAsync(string refreshToken);
+	}
+}
