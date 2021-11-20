23e5f398103facca60917e38a700be801ae02f49
Rajmundo1 <rajmund.knapp@gmail.com>
2021-11-20 16:17:39 +0100

    Add Automapper profiles

---
+++ src/backend/CryptoDoge.BLL/MapperProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CryptoDoge.BLL.Dtos;
+using CryptoDoge.Model.DataTransferModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoDoge.BLL
+{
+	public class MapperProfiles: Profile
+	{
+		public MapperProfiles()
+		{
+			CreateMap<RegisterDto, RegisterData>();
+		}
+
+	}
+}
