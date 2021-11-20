bbe3e1100b2ec8afcd3dfd5c34d5e9046a928382
Rajmundo1 <rajmund.knapp@gmail.com>
2021-11-20 16:16:44 +0100

    Add Repository Interfaces

---
+++ src/backend/CryptoDoge.Model/Interfaces/IAuthRepository.cs
@@ -0,0 +1,18 @@
+﻿using CryptoDoge.Model.DataTransferModels;
+using CryptoDoge.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoDoge.Model.Interfaces
+{
+    public interface IAuthRepository
+    {
+        Task RegisterAsync(RegisterData registerData);
+        Task<User> GetUserByRefreshTokenAsync(string refreshToken);
+        Task RemoveRefreshTokenAsync(string userId);
+        Task SaveRefreshTokenAsync(User user, string refreshToken);
+    }
+}
