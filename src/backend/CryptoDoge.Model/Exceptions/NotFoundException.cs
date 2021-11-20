ae70eb8055bc2a835b5327a7f5c29be08411a990
Rajmundo1 <rajmund.knapp@gmail.com>
2021-11-20 16:15:35 +0100

    Add Models + Exceptions

---
+++ src/backend/CryptoDoge.Model/Exceptions/NotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoDoge.Model.Exceptions
+{
+	public class NotFoundException : Exception
+	{
+		public int? ErrorCode { get; set; }
+
+		public NotFoundException(string message, int? errorCode = null) : base(message)
+		{
+			ErrorCode = errorCode;
+		}
+	}
+}
