fea14b6c823bfc460ac42660b0b694d433866f50
Rajmundo1 <rajmund.knapp@gmail.com>
2021-11-20 16:19:47 +0100

    Add Controllers + IdentityService implementation

---
+++ src/backend/CryptoDoge.Server/Infrastructure/Services/IdentityService.cs
@@ -0,0 +1,36 @@
+﻿using CryptoDoge.Model.Entities;
+using CryptoDoge.Model.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CryptoDoge.Server.Infrastructure.Services
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly HttpContext context;
+        private readonly UserManager<User> userManager;
+
+        public IdentityService(IHttpContextAccessor httpContextAccessor,
+                                UserManager<User> userManager)
+        {
+            this.context = httpContextAccessor.HttpContext;
+            this.userManager = userManager;
+        }
+
+        public async Task<User> GetCurrentUserAsync()
+        {
+            var id = context.User.Claims.Single(x => x.Type == ClaimTypes.NameIdentifier).Value;
+            return await userManager.FindByIdAsync(id);
+        }
+
+        public Task<string> GetCurrentUserIdAsync()
+        {
+            return Task.FromResult(context.User.Claims.SingleOrDefault(x => x.Type == "id")?.Value);
+        }
+    }
+}
