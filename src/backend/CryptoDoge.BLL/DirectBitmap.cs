bbdcd56f6bab880aae48e3eec532010f3b901689
Kovacs Akos <akoskovacs1980@gmail.com>
2021-12-02 11:24:35 +0100

    Implemented Dispose correctly in DirectBitmap

--- src/backend/CryptoDoge.BLL/DirectBitmap.cs
+++ src/backend/CryptoDoge.BLL/DirectBitmap.cs
@@ -7,9 +7,10 @@ namespace CryptoDoge.BLL
 {
     internal class DirectBitmap : IDisposable
     {
+        private bool disposedValue;
+
         public Bitmap Bitmap { get; private set; }
         public int[] Bits { get; private set; }
-        public bool Disposed { get; private set; }
         public int Height { get; private set; }
         public int Width { get; private set; }
 
@@ -41,12 +42,31 @@ namespace CryptoDoge.BLL
             return result;
         }
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // dispose managed state (managed objects)
+                    Bitmap.Dispose();
+                    BitsHandle.Free();
+                }
+
+                // free unmanaged resources (unmanaged objects) and override finalizer
+
+                // set large fields to null
+                Bitmap = null;
+                Bits = null;
+                disposedValue = true;
+            }
+        }
+
         public void Dispose()
         {
-            if (Disposed) return;
-            Disposed = true;
-            Bitmap.Dispose();
-            BitsHandle.Free();
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
